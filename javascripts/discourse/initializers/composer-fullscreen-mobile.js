2e000b51d950c3a7ff830f5e283e9fbcec8ad9a1
fearlessfrog <fearlessfrog@zoomread.com>
2026-06-08 12:07:34 -0700

    Update composer-fullscreen-mobile.js

--- javascripts/discourse/initializers/composer-fullscreen-mobile.js
+++ javascripts/discourse/initializers/composer-fullscreen-mobile.js
@@ -1,11 +1,13 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer((api) => {
-  api.modifyClass("component:composer-toggles", {
-    pluginId: "composer-fullscreen-mobile",
-
-    get showFullScreenButton() {
-      return !this.args.disableTextarea;
-    },
-  });
+  api.modifyClass(
+    "component:composer-toggles",
+    (Superclass) =>
+      class extends Superclass {
+        get showFullScreenButton() {
+          return !this.args.disableTextarea;
+        }
+      }
+  );
 });
